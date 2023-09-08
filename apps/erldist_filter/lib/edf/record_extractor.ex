c6574885cd6bd84988112381de713c9bb8e482bf
Andrew Bennett <potatosaladx@meta.com>
2023-09-08 11:56:18 -0400

    Add headers to Elixir files.

--- apps/erldist_filter/lib/edf/record_extractor.ex
+++ apps/erldist_filter/lib/edf/record_extractor.ex
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# Copyright (c) WhatsApp LLC
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE.md file in the root directory of this source tree.
+
 defmodule EDF.RecordExtractor do
   @moduledoc false
 
